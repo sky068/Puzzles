9031c6800b08b94f5a064361e54c7637726bd07f
xujw <xujw@topgame.com>
2015-12-29 16:03:47 +0800

    修正安卓切图不能使用大图的问题，改成Uri裁剪

--- proj.android/src/org/cocos2dx/cpp/ImagePicker.java
+++ proj.android/src/org/cocos2dx/cpp/ImagePicker.java
@@ -1,28 +1,15 @@
 package org.cocos2dx.cpp;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.UUID;
-
-import org.cocos2dx.lib.Cocos2dxHelper;
-
-import android.R;
 import android.app.Activity;
-import android.content.ContentResolver;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.provider.MediaStore.Audio.Media;
 import android.util.Log;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 
 public class ImagePicker {
     
@@ -34,12 +21,14 @@ public class ImagePicker {
     
     private static ImagePicker instance = null;
     private static Activity activity = null;
+    private static String TAG = "ImagePicker";
     
     public static native void onImageSaved(String path);
     
     //拍摄照片保存路径
     private static String savePath = Environment.getExternalStorageDirectory() +"/PuzzlesImg";
     private static String photoName = "";
+    private static Uri imgUri = null;
     
     public static ImagePicker getInstance()
     {
@@ -62,21 +51,21 @@ public class ImagePicker {
         Intent intent = new Intent(Intent.ACTION_PICK, null);
         intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_UNSPECIFIED);
         activity.startActivityForResult(intent, PHOTOZOOM);
-        
     }
     
     //打开相机
     static public void openCamera()
     {
     	File destDir = new File(savePath);
-    	  if (!destDir.exists()) 
-    	  {
-    		  destDir.mkdirs();
-    	  }
-    	photoName = System.currentTimeMillis() + ".jpg";
-
+    	if (!destDir.exists()) 
+        {
+    		destDir.mkdirs();
+        }
+    	photoName = "temp.jpg";
+    	File file = new File(savePath + "/" + photoName);
+    	imgUri = Uri.fromFile(file);
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(savePath,photoName)));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
         activity.startActivityForResult(intent, PHOTOHRAPH);
     }
     
@@ -86,128 +75,77 @@ public class ImagePicker {
         if (resultCode == NONE)
             return;
         
-        // 拍照
+        // 拍照 不能使用data，因为没有返回是空的
         if (requestCode == PHOTOHRAPH)
         {
-//        	File picture = new File(Environment.getExternalStorageDirectory(),"/cc_puzzles.jpg");
-//            startPhotoZoom(Uri.fromFile(picture));
-        	
-        	//不再用上面的代码裁剪了，直接返回路径
-        	String fileDir = savePath + "/" + photoName;
-        	onImageSaved(fileDir);
+        	if (imgUri == null)
+        	{
+        		Log.e(TAG, "PHOTOHRAPH imgUri is null");
+        		return;
+			}
+            startPhotoZoom(imgUri);
         }
         
-        if (data == null)
-            return;
-        
-        // 读取相册缩放图片
         if (requestCode == PHOTOZOOM)
         {
-        	//不再裁剪
-//            startPhotoZoom(data.getData());
-   
-        	getBitmapPath(data);
+        	 // 读取相册缩放图片
+            if (data==null )
+            {
+            	Log.e(TAG, "data is null");
+                return;
+            }
+            if (data.getData()==null) 
+            {
+            	Log.e(TAG, "data.getData() is null");
+                return;
+			}
+            startPhotoZoom(data.getData());
         }
         
         // 处理结果
         if (requestCode == PHOTORESOULT)
         {
-            Bundle extras = data.getExtras();
-            if (extras != null) {
-                Bitmap photo = extras.getParcelable("data");
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                photo.compress(Bitmap.CompressFormat.JPEG, 90, stream);
-                
-                // XXX/@ci_8888-8888-8888-8888.jpg
-                //String path = activity.getFilesDir() + "/@ci_" + UUID.randomUUID().toString() + ".jpg";
-                String fileName = UUID.randomUUID().toString() + ".jpg";
-                String path = Environment.getExternalStorageDirectory()+"/" + fileName;
-                saveMyBitmap(path, photo);
-                
-                // 通知C++层已保存图片 并返回路径
-                Log.e("ImagePicker", "图片已经保存，通知c++层，");
-                onImageSaved(path);
-            }
+        	Bitmap bitmap = decodeUriAsBitmap(imgUri);
+        	if (bitmap == null)
+        	{
+        		Log.e(TAG, "bitmap is null");
+			}
+        	Log.e("ImagePicker", "图片已经保存，通知c++层，");
+        	onImageSaved(savePath + "/" + photoName);
         }
-    }
-    
-    public void getBitmapPath(Intent data)
-    {
-    	Bitmap bm = null;
-    	ContentResolver resolver = activity.getContentResolver();
-    	try {
-			Uri originUri = data.getData();	//获取图片的uri
-			bm = MediaStore.Images.Media.getBitmap(resolver, originUri);	//得到bitmap图片
-			//下面开始获取路径
-			String[] proj = {MediaStore.Images.Media.DATA};
-			Cursor cursor = activity.managedQuery(originUri, proj, null, null, null);
-			int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-			cursor.moveToFirst();
-			//最后根据索引获取图片路径
-			String path = cursor.getString(column_index);
-			 // 通知C++层已保存图片 并返回路径
-            Log.e("ImagePicker", "图片已经保存，通知c++层，");
-            onImageSaved(path);
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-			Log.e("Puzzles", e.toString());
-		}
     }
     
     public void startPhotoZoom(Uri uri)
     {
+    	photoName = System.currentTimeMillis() + ".jpg";
+    	File file = new File(savePath,photoName);
+    	imgUri = Uri.fromFile(file);
+    	
         Intent intent = new Intent("com.android.camera.action.CROP");
         intent.setDataAndType(uri, IMAGE_UNSPECIFIED);
         intent.putExtra("crop", "true");
         intent.putExtra("aspectX", 1);
         intent.putExtra("aspectY", 1);
-        intent.putExtra("outputX", 100);
-        intent.putExtra("outputY", 100);
-        intent.putExtra("return-data", true);
-        activity.startActivityForResult(intent, PHOTORESOULT);
-        
-        //截取大图
-//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT,null);
-//        intent.setType(IMAGE_UNSPECIFIED);
-//        intent.setData(uri);
-//        intent.putExtra("crop", "true");
-//        intent.putExtra("aspectX", 1);
-//        intent.putExtra("aspectY", 1);
-//        intent.putExtra("outputX", 600);
-//        intent.putExtra("outputY", 600);
-//        intent.putExtra("scale", "true");
-//        intent.putExtra("return-data", false);
-//        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-//        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
-//        intent.putExtra("noFaceDetection", true);
+        intent.putExtra("outputX", 600);
+        intent.putExtra("outputY", 600);
+        intent.putExtra("return-data", false);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+
         activity.startActivityForResult(intent, PHOTORESOULT);
     }
     
-    public void saveMyBitmap(String path,Bitmap photo)
-    {
-        File f = new File(path);
-        try {
-            f.createNewFile();
-        } catch (IOException e) {
-        }
-        FileOutputStream fOut = null;
-        try {
-            fOut = new FileOutputStream(f);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        photo.compress(Bitmap.CompressFormat.JPEG, 90, fOut);
-        try {
-            fOut.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            fOut.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    private Bitmap decodeUriAsBitmap(Uri uri)
+	{
+		Bitmap bitmap = null;
+		try {
+			bitmap = BitmapFactory.decodeStream(activity.getContentResolver().openInputStream(uri));
+		} catch (FileNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return null;
+		}
+		
+		return bitmap;
+	}
     
 }
